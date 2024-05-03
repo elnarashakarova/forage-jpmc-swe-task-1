abe60ba150816ddacd305539c49b84c0d4a85542
Elnara Shakarova <elnarashakarova@gmail.com>
2024-05-03 07:05:03 +0200

    Feat: Added assertions to test cases for getDataPoint and getRatio functions in client test file.

--- client_test.py
+++ client_test.py
@@ -1,5 +1,5 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint, getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,6 +8,10 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    expected_price = (120.48 + 121.2) / 2
+        # Calculate the expected price directly based on input quotes
+    actual_price = getDataPoint(quotes[0])[3]
+    self.assertEqual(actual_price, expected_price)
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,10 +19,22 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
-
-
+    expected_price = (117.87 + 121.68) / 2
+    # Calculate the expected price directly based on input quotes
+    actual_price = getDataPoint(quotes[1])[3]
+    self.assertEqual(actual_price, expected_price)
+    
   """ ------------ Add more unit tests ------------ """
-
+       
+  def test_getRatio_positivePrices(self):
+      # Test with positive price values
+      ratio = getRatio(20, 10)
+      self.assertEqual(ratio, 2)
+      
+  def test_getRatio_negativePrices(self):
+      # Test with negative price values
+      ratio = getRatio(-20, 10)
+      self.assertEqual(ratio, -2)
 
 
 if __name__ == '__main__':
